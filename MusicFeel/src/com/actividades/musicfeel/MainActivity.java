c8af526baea57215f6a69f82a72a5a0a34664bf8
rjgp1993 <rjgp1993@gmail.com>
2014-02-16 19:00:42 +0100

    Subida Día 16-2-

--- MusicFeel/src/com/actividades/musicfeel/MainActivity.java
+++ MusicFeel/src/com/actividades/musicfeel/MainActivity.java
@@ -3,14 +3,35 @@ package com.actividades.musicfeel;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	Button boton_archivo;
+	Button boton_grabar;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		boton_archivo= (Button) findViewById(R.id.boton_archivo);
+		boton_grabar= (Button) findViewById(R.id.boton_grabacion);
 		
+		boton_archivo.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				pulsaArchivo();
+				
+			}
+		});
+		
+		boton_grabar.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				pulsaGrabar();
+				
+			}
+		});
 	}
 
 	@Override
@@ -18,6 +39,13 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void pulsaArchivo(){
+		
+	}
+	public void pulsaGrabar(){
+		
 	}
 
 }
